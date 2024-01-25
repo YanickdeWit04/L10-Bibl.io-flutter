d5ebb3d88935dd463275a18c00125a91c3f655b0
kbonthui0324 <kbonthui0324@student.landstede.nl>
2024-01-25 11:52:25 +0100

    Navbar pushed

---
+++ lib/Navbar.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const Navbar());
+
+class Navbar extends StatelessWidget {
+  const Navbar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: NavbarExample(),
+    );
+  }
+}
+
+class NavbarExample extends StatefulWidget {
+  const NavbarExample({Key? key}) : super(key: key);
+
+  @override
+  State<NavbarExample> createState() => _NavbarExampleState();
+}
+
+class _NavbarExampleState extends State<NavbarExample> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Catalog',
+      style: optionStyle,
+    ),
+    Text(
+      'Hallo je dikke vader'
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Catalog',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.plus_one),
+            label: 'Add books',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.barcode_reader),
+            label: 'Barcode Scanner',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.purple[600],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:biblio/CatalogPage.dart';
 import 'package:flutter/material.dart';
+import 'package:biblio/Navbar.dart';
 
 void main() {
   runApp(MyApp());
@@ -6,81 +8,10 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+    Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          iconTheme: IconThemeData(color: Colors.black),
-          // Icon color in app bar
-        ),
-        bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          selectedItemColor: Colors.black,
-          // Selected item color in navigation bar
-          unselectedItemColor: Colors.black
-              .withOpacity(0.6), // Unselected item color in navigation bar
-        ),
-      ),
-      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      home: Navbar(),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _currentIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          PopupMenuButton<String>(
-            onSelected: (value) {
-              // Handle profile dropdown menu item selection
-              // You can add your logic here
-            },
-            itemBuilder: (BuildContext context) {
-              return [
-                const PopupMenuItem(
-                  value: 'profile',
-                  child: Text('Profile'),
-                ),
-                // Add more menu items as needed
-              ];
-            },
-          ),
-        ],
-      ),
-      body: Center(
-        child: Text('Content for selected tab $_currentIndex'),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (index) {
-          // Handle tab navigation
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.local_library),
-            label: 'Catalog',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-      ),
-    );
   }
-}
