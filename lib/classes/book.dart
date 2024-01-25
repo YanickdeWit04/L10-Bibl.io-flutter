01ede45414866e263352eb810b55af5bfe2b4c73
Yanick <yanickdewit@gmail.com>
2024-01-25 11:13:38 +0100

    show a hard coded book

---
+++ lib/classes/book.dart
@@ -0,0 +1,17 @@
+class Book{
+    String title;
+    String isbn;
+    String author;
+    String discription;
+    String cover;
+    String status;
+
+    Book({
+        required this.title,
+        required this.isbn,
+        required this.author,
+        required this.discription,
+        required this.cover,
+        required this.status
+    });
+}
