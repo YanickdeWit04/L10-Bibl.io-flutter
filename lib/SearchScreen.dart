6d6235d16f7eddd6f3d89cd655867c502ac458b7
Student Rick Fijn <rfijn9415@student.landstede.nl>
2024-01-26 10:26:45 +0100

    added qr code and search function

---
+++ lib/SearchScreen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatefulWidget {
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  List<String> data = [
+    'Boek',
+    'Boek1',
+    'Boek2',
+    'Boek3',
+    'Boek4',
+    'Boek5',
+    'Boek6',
+    'Boek7',
+    'Boek8',
+    'Boek9',
+  ];
+
+  List<String> searchResults = [];
+
+  void onQueryChanged(String query) {
+    setState(() {
+      searchResults = data
+          .where((item) => item.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      ),
+      body: Column(
+        children: [
+          SearchBar(onQueryChanged: onQueryChanged),
+          Expanded(
+            child: ListView.builder(
+              itemCount: searchResults.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(searchResults[index]),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SearchBar extends StatefulWidget {
+  final ValueChanged<String> onQueryChanged;
+
+  SearchBar({required this.onQueryChanged});
+
+  @override
+  _SearchBarState createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  String query = '';
+
+  void onQueryChanged(String newQuery) {
+    setState(() {
+      query = newQuery;
+      widget.onQueryChanged(query); // Doorgeven aan de bovenliggende widget
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: TextField(
+        onChanged: onQueryChanged,
+        decoration: InputDecoration(
+          labelText: 'Search',
+          border: OutlineInputBorder(),
+          prefixIcon: Icon(Icons.search),
+        ),
+      ),
+    );
+  }
+}
